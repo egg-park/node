c3e782187e52f141021c77fc5c3fe7e402bbb0d4
egg-park <hyeran.park1211@gmail.com>
2021-12-16 22:45:54 +0900

    로그아웃 까지 완료

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const {User} = require("./models/User");
 const config = require("./config/key");
+const {User} = require("./models/User");
+const {auth} = require('./middleware/auth');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 //appolication/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -31,6 +33,57 @@ app.post('/register', (req,res) => {
       success : true
     })
   });
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({email : req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess : false,
+        message:"제공된 이메일에 해당하는 유저가 없습니다."
+    })
+  }
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        
+        // 토큰을 저장한다. 
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginsuccess:true, userId: user._id})
+      })
+    })
+  })
+})
+
+app.get('/api/users/auth', auth, (req, res) =>{
+  //여기 로직을 타는것은 미들웨어 auth를 통과 했다는 것 (Auth = true)
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name : req.user.name,
+    lastname : req.user.lastname,
+    role : req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) =>{
+  User.findOneAndUpdate({_id:req.user._id}, 
+    {token:""},
+    (err,user)=>{
+      if(err) return res.status(200).send({
+        success:true
+      })
+    })
 })
 
 app.listen(port, () => {
